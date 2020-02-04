b7d09979b8817845a024249faa06e8fd39fd296d
Wasim Khan <wasimshan786@gmail.com>
2020-02-04 15:27:59 +0530

    customer onboarding form done

--- src/app/customer-onboarding-form/customer-onboarding-form.component.ts
+++ src/app/customer-onboarding-form/customer-onboarding-form.component.ts
@@ -17,13 +17,16 @@ export class CustomerOnboardingFormComponent implements OnInit {
   public primaryCol: number;
   public documentsCol: number;
   public contractorInfoCol: number;
+  public checkboxCol: number;
+  public buttonTopMargin: number;
+  public buttonBottomMargin: number;
 
   public fontSize: number;
   public spanText: number;
   public formMargin: number;
   public titleMargin: number;
 
-  public isXsmall: boolean = false;
+  public isXsmall: boolean;
   public isSmall: boolean;
   public isMedium: boolean;
   public isLarge: boolean;
@@ -47,11 +50,14 @@ export class CustomerOnboardingFormComponent implements OnInit {
           this.primaryCol = 1;
           this.documentsCol = 1;
           this.contractorInfoCol = 1;
+          this.checkboxCol = 1;
 
           this.fontSize = 28;
           this.spanText = 20;
           this.formMargin = 30;
           this.titleMargin = 50;
+          this.buttonTopMargin = 120;
+          this.buttonBottomMargin = 50;
         } else if (state.breakpoints[Breakpoints.Small]) {
           this.isXsmall = false;
           this.isSmall = true;
@@ -61,10 +67,13 @@ export class CustomerOnboardingFormComponent implements OnInit {
           this.primaryCol = 2;
           this.documentsCol = 2;
           this.contractorInfoCol = 2;
+          this.checkboxCol = 1;
 
           this.fontSize = 30;
           this.formMargin = 75;
           this.titleMargin = 60;
+          this.buttonTopMargin = 90;
+          this.buttonBottomMargin = 50;
         } else if (state.breakpoints[Breakpoints.Medium]) {
           this.isXsmall = false;
           this.isSmall = false;
@@ -74,10 +83,13 @@ export class CustomerOnboardingFormComponent implements OnInit {
           this.primaryCol = 2;
           this.documentsCol = 2;
           this.contractorInfoCol = 2;
+          this.checkboxCol = 2;
 
           this.fontSize = 36;
           this.formMargin = 97;
           this.titleMargin = 80;
+          this.buttonTopMargin = 90;
+          this.buttonBottomMargin = 30;
         } else {
           this.isXsmall = false;
           this.isSmall = false;
@@ -87,10 +99,13 @@ export class CustomerOnboardingFormComponent implements OnInit {
           this.primaryCol = 4;
           this.documentsCol = 3;
           this.contractorInfoCol = 2;
+          this.checkboxCol = 3;
 
           this.fontSize = 36;
           this.formMargin = 97;
           this.titleMargin = 80;
+          this.buttonTopMargin = 50;
+          this.buttonBottomMargin = 20;
         }
       });
   }
@@ -102,6 +117,16 @@ export class CustomerOnboardingFormComponent implements OnInit {
       "line-height": `${this.fontSize}px`,
       "margin-top": `${this.titleMargin}px`,
       "margin-bottom": `${this.titleMargin}px`
+    };
+  }
+
+  setCheckBoxStyles() {
+    return {
+      "font-size": `${this.fontSize}px`,
+      height: `${this.fontSize}px`,
+      "line-height": `${this.fontSize}px`,
+      "margin-top": `${this.titleMargin}px`,
+      "margin-bottom": `${this.titleMargin - 20}px`
     };
   }
 }
